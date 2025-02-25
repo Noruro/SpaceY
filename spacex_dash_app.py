1bde1c6bfcd385d77af37afce123763015aa1bd4
Noruro <norbertosfc@gmail.com>
2025-02-25 12:49:00 +0100

    spacex_dash_app.py

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -1,5 +1,3 @@
-# Import required libraries
-
 import pandas as pd
 import dash
 import dash_html_components as html
@@ -24,22 +22,19 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                         style={'textAlign': 'center', 'color': '#503D36',
                                                'font-size': 40}),
                                 # TASK 1: Add a dropdown list to enable Launch Site selection
-                                # The default select value is for ALL sites
                                 dcc.Dropdown(id='site-dropdown',
                                             options=options,
-                                            value='ALL',  # Valor por defecto
+                                            value='ALL', 
                                             placeholder="Select a Launch Site here",
                                             searchable=True),
                                 html.Br(),
 
                                 # TASK 2: Add a pie chart to show the total successful launches count for all sites
-                                # If a specific launch site was selected, show the Success vs. Failed counts for the site
                                 html.Div(dcc.Graph(id='success-pie-chart')),
                                 html.Br(),
                                 html.P("Payload range (Kg):"),
 
                                 # TASK 3: Add a slider to select payload range
-                                #dcc.RangeSlider(id='payload-slider',...)
                                 dcc.RangeSlider(id='payload-slider',
                                                 min=0, max=10000, step=1000,
                                                 marks={i: str(i) for i in range(0, 10001, 2500)},
@@ -66,7 +61,6 @@ def update_pie_chart(selected_site):
     return fig
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
-# Callback para actualizar el gráfico de dispersión
 @app.callback(
     Output(component_id='success-payload-scatter-chart', component_property='figure'),
     [Input(component_id='site-dropdown', component_property='value'),
